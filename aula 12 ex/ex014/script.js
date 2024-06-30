5f8f1878d3905706e0f041796702b23c99e2453f
Pedro Henrique <164812419+pedrohendev@users.noreply.github.com>
2024-06-30 19:37:37 -0300

    exercicio de javascript

---
+++ aula 12 ex/ex014/script.js
@@ -0,0 +1,21 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas!`
+    if (hora >= 0 && hora < 12) {
+        // bom dia
+        img.src = 'imagem/manhã.png'
+        document.body.style.background = '#fcaa56'
+    } else if (hora >= 12 && hora < 18) {
+        // boa tarde
+        img.src = 'imagem/tarde.png'
+        document.body.style.background = '#cf8779'
+
+    } else {
+        // boa noite
+        img.src = 'imagem/noite.png'
+        document.body.style.background = '#354159'
+    }
+}
